cd862c5228ea6503b73fd923cff5288e07f98ea1
2006uday <142423485+2006uday@users.noreply.github.com>
2026-03-31 12:01:07 +0530

    image added

--- components/expertise-services/myServices.ts
+++ components/expertise-services/myServices.ts
@@ -3,7 +3,7 @@ export const myServices = [
     id: "01",
     slug: "industrial-design",
     title: "INDUSTRIAL DESIGN",
-    image: "/home-services/living-area-1.jpeg",
+    image: "/home-services/living-area-01.jpeg",
     description:
       "Welcome to the Modern Urban Retreat project, where we are tasked with transforming a 3-bedroom apartment in the heart of Metropolisville into a stylish and functional living space for the Smith family...",
   },
@@ -11,7 +11,7 @@ export const myServices = [
     id: "02",
     slug: "corporate-design",
     title: "CORPORATE DESIGN",
-    image: "/home-services/service-3.jpeg",
+    image: "/home-services/service-03.jpeg",
     description:
       "We design functional and elegant 3D wall solutions that optimize space while maintaining a sleek, contemporary look...",
   },
@@ -19,7 +19,7 @@ export const myServices = [
     id: "03",
     slug: "residential-design",
     title: "RESIDENTIAL DESIGN",
-    image: "/home-services/service-2.jpeg",
+    image: "/home-services/service-02.jpeg",
     description:
       "Custom furniture crafted to fit your lifestyle, blending comfort, durability, and timeless design...",
   },
@@ -27,7 +27,7 @@ export const myServices = [
     id: "04",
     slug: "custom-furniture",
     title: "CUSTOM FURNITURE",
-    image: "/home-services/service-1.jpeg",
+    image: "/home-services/service-01.jpeg",
     description:
       "Lighting solutions that enhance mood, improve functionality, and elevate architectural features...",
   },
